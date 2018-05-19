53f1b362f55ac2ed1ed9c11c6877d061d3281f03
Irgit <36793333+tarbagan@users.noreply.github.com>
2018-05-19 09:32:45 +0700

    Update wiki_lat_city.py

--- wiki_lat_city.py
+++ wiki_lat_city.py
@@ -4,14 +4,16 @@ from geopy.geocoders import Nominatim
 import time
 import wikipedia
 import csv
+import re
+from geopy.distance import vincenty
 
 def get_city(city=True):
     getCitiesURL = {}
     getCitiesURL["vk_getCities"] = "https://api.vk.com/method/database.getCities.json?"
-    getCitiesURL["country_id"] = "country_id=1" #ID Country
-    getCitiesURL["region_id"] = "region_id=1157049" #ID Region 1157049 - Tuva Republic
+    getCitiesURL["country_id"] = "country_id=1" #1 - Russian VK_Api
+    getCitiesURL["region_id"] = "region_id=1157049" #1157049 - Republic of Tuva VK_Api
     getCitiesURL["count"] = "count1000"
-    getCitiesURL["token"] = "VK_TOKEN"
+    getCitiesURL["token"] = "access_token=YOU_TOKEN_VK"
     getCitiesURL["version_vkapi"] = "v=5.52"
     try:
         urlcity = ("&".join(getCitiesURL.values()))
@@ -33,19 +35,26 @@ def lat_lot():
             cit = cit
         else:
             cit["area"] = "Город"
-        location = geolocator.geocode( cit["title"], timeout=3 )
+        namegorod = cit["title"]+", Тыва"
+        location = geolocator.geocode(namegorod, timeout=3 )
         if location:
             lat = location.latitude
             lot = location.longitude
             cit["latitude"] = lat
             cit["longitude"] = lot
+            newport_ri = (51.705380, 94.444599) #Kyzyl
+            cleveland_oh = (lat, lot)
+            cit["Kyzyl"] = round(vincenty(newport_ri, cleveland_oh ).kilometers)
         else:
             lat = ""
             lot = ""
             cit["latitude"] = lat
             cit["longitude"] = lot
+            cit["Kyzyl"] = ""
         try:
-            page = wikipedia.summary(cit["title"])
+            namegorod = cit["title"] + ", Тыва"
+            page = wikipedia.summary(namegorod)
+            page = re.sub("\n"," ",page)
             if page:
                 cit["info"] = page
             else:
@@ -60,12 +69,12 @@ def lat_lot():
 
 def record():
     with open( "d:/AnacodaProgect/gorod_cit.csv", "w", encoding='utf-8' ) as file:
-        fieldnames = ['id', 'title', 'area', 'region', 'latitude', 'longitude', 'info']
+        fieldnames = ['id', 'title', 'area', 'region', 'latitude', 'longitude', 'kyzyl_km', 'info']
         writer = csv.DictWriter( file,  delimiter='¦', fieldnames=fieldnames )
         writer.writeheader()
         for reccit in lat_lot():
             print (reccit)
             print ("ok")
-            writer.writerow({'id': reccit["id"], 'title': reccit["title"], 'area': reccit["area"], 'region': reccit["region"], 'latitude': reccit["latitude"], 'longitude': reccit["longitude"],'info': reccit["info"]})
+            writer.writerow({'id': reccit["id"], 'title': reccit["title"], 'area': reccit["area"], 'region': reccit["region"], 'latitude': reccit["latitude"], 'longitude': reccit["longitude"], 'kyzyl_km': reccit["Kyzyl"],'info': reccit["info"]})
 
 record()
